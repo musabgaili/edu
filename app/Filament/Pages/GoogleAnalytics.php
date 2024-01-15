0e2c37c6c8850eebe8144edd62369a2a953c74fd
elmu35b <musabgaili@gmail.com>
2024-01-16 01:28:13 +0300

    google analytics

---
+++ app/Filament/Pages/GoogleAnalytics.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+use BezhanSalleh\FilamentGoogleAnalytics\Widgets;
+class GoogleAnalytics extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.google-analytics';
+
+    
+ 
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            Widgets\PageViewsWidget::class,
+            Widgets\VisitorsWidget::class,
+            Widgets\ActiveUsersOneDayWidget::class,
+            Widgets\ActiveUsersSevenDayWidget::class,
+            Widgets\ActiveUsersTwentyEightDayWidget::class,
+            Widgets\SessionsWidget::class,
+            Widgets\SessionsDurationWidget::class,
+            Widgets\SessionsByCountryWidget::class,
+            Widgets\SessionsByDeviceWidget::class,
+            Widgets\MostVisitedPagesWidget::class,
+            Widgets\TopReferrersListWidget::class,
+        ];
+    }
+}
